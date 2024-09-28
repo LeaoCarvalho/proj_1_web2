631164962fcd3fdf72ae7c3993dd95e586c55c89
LeaoCarvalho <matheus.eu.pessoal@gmail.com>
2024-09-28 16:37:25 -0300

    Build: build

---
+++ src/main/java/com/example/proj_1_web2/Proj1Web2Application.java
@@ -0,0 +1,13 @@
+package com.example.proj_1_web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj1Web2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj1Web2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proj_1_web2/Proj1Web2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proj_1_web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj1Web2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
